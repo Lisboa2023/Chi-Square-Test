829065fb2c688c42a57eeb22453e2db1e37a4dcf
Lisboa2023 <davi.pina@itec.ufpa.br>
2024-05-06 21:35:35 -0300

    Update

--- main.cpp
+++ main.cpp
@@ -24,19 +24,11 @@ int main()
     return 0;
 }
 
-<<<<<<< HEAD
-void ChiSquareTest(int size, int df, float *v){
-    ProbabilityDistribution X2(size);
-    
-    X2.setValues(v);    
-    X2.setDistribution(size);
-=======
 void ChiSquareTest(int size, int df, float *measurement, float* mEstimated, double *covarianceMatrix){
     ProbabilityDistribution X2(size,df);
 
     X2.setValues(measurement, mEstimated, covarianceMatrix);    
     X2.setDistribution();
->>>>>>> patch_1
     float d = X2.getDistribution();
     X2.setCumulativeDistributionFunction(0, d);
     X2.print();
